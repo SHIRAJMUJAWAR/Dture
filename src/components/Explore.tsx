ce016f89a65fb1c5b6fdacc001941945d1d9c1b3
SHIRAJMUJAWAR <shirajmujawar03@gmail.com>
2025-10-26 09:35:09 +0530

    chore: enable Tailwind and import global CSS (index.css, tailwind config)

--- src/components/Explore.tsx
+++ src/components/Explore.tsx
@@ -7,11 +7,11 @@ const Explore = () => {
      <div>    
        <div  className="container  ">
     <div>
-          <div className="mt-3xw-250 bg-black mt-19 rounded-4xl  text-2xl ml-52 mr-52  border-gray-600 flex flex-row   gap-3"> 
+          <div className="mt-3x w-250 bg-black mt-19 rounded-4xl  text-2xl ml-52 mr-52  border-gray-600 flex flex-row   gap-3"> 
        
       <CiSearch size={40} className="  m-2 absolute mr-20 "/>
          <input
-          className=" bg-gray-900 pl-13   h-15 w-220 rounded-4xl border-gray-600"
+          className=" bg-gray-900 pl-13   h-15 w-250 rounded-4xl border-gray-600"
         placeholder="  Search debates,rooms,users "
     
       />
